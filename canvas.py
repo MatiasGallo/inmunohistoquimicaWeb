9de4fc7908fccacbab60ab045de45f9e2437d046
matias-gallo <matias.a.gallo.96@gmail.com>
2021-08-22 15:09:49 -0300

    Eliminados comentarios innecesarios

--- canvas.py
+++ canvas.py
@@ -19,41 +19,25 @@ stroke_width = 1
 bg_image = st.sidebar.file_uploader("Image:", type=["png", "jpg"])
 
 def click_event(event, x, y, flags, params):
-  
-    # checking for left mouse clicks
     if (event == cv2.EVENT_LBUTTONDOWN) or (event==cv2.EVENT_RBUTTONDOWN):
-  
-        # displaying the coordinates
-        # on the Shell
-        #print(x, ' ', y)
         p = Coordinate(x,y)
         if (valid == True):
             valids.append(p)
         else:
             invalids.append(p)
-        # displaying the coordinates
-        # on the image window
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(RGB_img, '.' , ((x-5),y), font,
                     1, (0, 0, 0), 2)
         cv2.imshow('img', RGB_img)
 
 def switchValid(x):
-    #print(valid)
-    print(valids)
-    print(invalids)
     global valid
-    if (valid == True):
-        valid = False
-    else:
-        valid = True
+    valid = not valid
     pass
 
 if bg_image:
     image = Image.open(bg_image)
     w,h = image.size
-    #st.text(w)
-    #st.text(h)
     newsize = (int(w/2), int(h/2))
     newImage = image.resize(newsize)
     newW,newH = newImage.size
@@ -67,27 +51,18 @@ if bg_image:
         #Default width 600
         key="canvas",
     )
-    # Do something interesting with the image data and paths
+
     if canvas_result.json_data is not None:
         formas=pd.json_normalize(canvas_result.json_data["objects"])
         if len(formas) != 0: 
             st.dataframe(formas)
             left = formas['left'][0]
-            #st.text(left)
             top = formas['top'][0]
-            #st.text(top)
             width = formas['width'][0]
-            #st.text(width)
             endSquare=left + width
             height = formas['height'][0]
-            #st.text(height)
             highSquare = top + height
         if st.sidebar.button('Recortar'):
-            #Revisar la relacion y usar h y w originales
-            #st.text(int(inicTop))
-            #st.text(int(finWidth))
-            #st.text(int(inicHigh))
-            #st.text(int(finHigh))
             # plugin='matplotlib'
             skimg = io.imread(bg_image,plugin='matplotlib')
             if len(formas) != 0: 
@@ -98,14 +73,10 @@ if bg_image:
                 cropped = skimg[int(inicHigh):int(finHigh),int(inicTop):int(finWidth)]
                 img = img_as_ubyte(cropped)
                 RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                #st.image(skimg)
-                #st.image(cropped)
-                #st.image(RGB_img)
             else:
                 img = img_as_ubyte(skimg)
                 RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            #cv2.namedWindow('img')
             cv2.namedWindow('img', cv2.WINDOW_NORMAL)
             # create switch for ON/OFF functionality
             cv2.createTrackbar('0 : OFF \n1 : ON', 'img',0,1,switchValid)
