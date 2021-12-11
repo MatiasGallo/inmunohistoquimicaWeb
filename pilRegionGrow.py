6df6fa272ae7f5e6a64494123d2ed73306c3c49d
matias-gallo <matias.a.gallo.96@gmail.com>
2021-12-11 13:12:11 -0300

    Agregada statefullness, context

--- pilRegionGrow.py
+++ pilRegionGrow.py
@@ -3,10 +3,14 @@ from PIL import Image
 import streamlit as st
 import cv2
 from skimage import img_as_ubyte 
+from numpy import median
 
 def regiongrow(imageSrc,epsilon,start_point : list):
+    print('Calculando')
     Q = Queue()
     s = []
+
+    color = []
     
     print(start_point)
     for i in start_point:
@@ -16,7 +20,7 @@ def regiongrow(imageSrc,epsilon,start_point : list):
 
     st.image(image)
     while not Q.isEmpty():
-
+        
         t = Q.deque()
         x = t[0]
         y = t[1]
@@ -76,26 +80,38 @@ def on_mouse(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         print ('Seed: ' + str(x) + ', ' + str(y), img[y,x])
         clicks.append((x,y))
+        st.session_state['clicks'] = clicks
 
 
 bg_image = st.sidebar.file_uploader("Image:", type=["png", "jpg"])
+pil_image = None
+RGB_img = None
 
+clicks = []
 if bg_image:
     pil_image = Image.open(bg_image)
-    clicks = []
     img = img_as_ubyte(pil_image)
     RGB_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    st.image(pil_image)
+    
 
+if (st.sidebar.button('Seeds') and RGB_img.all()):
+    print ('1')
     cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-    # create switch for ON/OFF functionality
-    #cv2.createTrackbar('0 : OFF \n1 : ON', 'img',0,1,switchValid)
-    #cv2.cvtColor(imgG, cv2.COLOR_BGR2RGB)
+    cv2.cvtColor(RGB_img, cv2.COLOR_BGR2RGB)
     cv2.imshow('img', RGB_img)
     cv2.setMouseCallback('img',on_mouse)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+else:
+    print ('2')
 
-    st.image(pil_image)
+epsilon = st.sidebar.slider( "Brillo" , min_value=0 , max_value=1 , value=1 , step=None , format=None , key=None)
+if (st.sidebar.button('Region Grow')):
+    #st.image(pil_image)
     print ('Inicio RG')
-    st.image(regiongrow(pil_image,1.2,clicks))
+    st.image(regiongrow(pil_image,0,clicks))
     print ('fin RG')
+
+if st.sidebar.button('Ver Clicks [DevTool]'):
+    print (st.session_state['clicks'])
