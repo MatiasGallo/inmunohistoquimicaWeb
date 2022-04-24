ccff796ecffabe706138a98cee6c92373cea6b93
matias-gallo <matias.a.gallo.96@gmail.com>
2022-04-24 18:08:09 -0300

    Changed setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
+mkdir -p ~/.streamlit/echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
