07b78588ed8f1f335db77f3edc2951ae54b1b2dd
matias-gallo <matias.a.gallo.96@gmail.com>
2022-04-28 19:19:37 -0300

    Heroku changes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 mkdir -p ~/.streamlit/
-echo "[general]"  > ~/.streamlit/credentials.toml
-echo "email = \"matias.a.gallo.96@gmail.com\""  >> ~/.streamlit/credentials.toml
-echo "[server]"  > ~/.streamlit/config.toml 
-echo "headless = true"  >> ~/.streamlit/config.toml
-echo "port = $PORT"  >> ~/.streamlit/config.toml
-echo "enableCORS = true"  >> ~/.streamlit/config.toml
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
